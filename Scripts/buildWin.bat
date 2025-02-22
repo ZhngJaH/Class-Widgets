f07b27c9ebd3763cd085508946df48ae7604925b
Wenxuan Shen <bushigemen114@gmail.com>
2025-02-22 16:59:53 +0800

    Update buildWin.bat

--- Scripts/buildWin.bat
+++ Scripts/buildWin.bat
@@ -6,17 +6,17 @@ echo 安装依赖
 uv pip install -r requirements.txt
 uv pip install pyinstaller
 echo 打包
-pyinstaller main.py `
-  -w `
-  -i ./img/favicon.ico `
-  -n ClassWidgets `
-  --contents-directory . `
-  --add-data config:config `
-  --add-data audio:audio `
-  --add-data img:img `
-  --add-data ui:ui `
-  --add-data font:font `
-  --add-data view:view `
-  --add-data LICENSE:. `
-  --add-data plugins:plugins `
+pyinstaller main.py ^
+  -w ^
+  -i ./img/favicon.ico ^
+  -n ClassWidgets ^
+  --contents-directory . ^
+  --add-data config:config ^
+  --add-data audio:audio ^
+  --add-data img:img ^
+  --add-data ui:ui ^
+  --add-data font:font ^
+  --add-data view:view ^
+  --add-data LICENSE:. ^
+  --add-data plugins:plugins ^
   --hidden-import darkdetect
